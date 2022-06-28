3883b122320c3ee51f732ed2007085191ceec732
cxs <437993390@qq.com>
2022-06-28 16:37:11 +0800

    fix: 前端工程 设置不显示设置面板 settings right-panel

--- barren-ui/src/settings.js
+++ barren-ui/src/settings.js
@@ -8,7 +8,7 @@ module.exports = {
    * @type {boolean} true | false
    * @description Whether show the settings right-panel
    */
-  showSettings: true,
+  showSettings: false,
 
   /**
    * @type {boolean} true | false
