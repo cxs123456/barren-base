890f5acee06e35948117b461124c02bfcfd7c34f
cxs <437993390@qq.com>
2022-12-11 09:33:16 +0800

    fix: modify git ignore file and base entity

--- barren-core/barren-common/src/main/java/org/barren/core/tool/entity/BaseEntity.java
+++ barren-core/barren-common/src/main/java/org/barren/core/tool/entity/BaseEntity.java
@@ -1,7 +1,9 @@
 package org.barren.core.tool.entity;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -16,20 +18,28 @@ import java.time.LocalDateTime;
 @Data
 public class BaseEntity implements Serializable {
 
+    @ApiModelProperty("ID")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
     @ApiModelProperty("创建人")
-    @TableField(fill = FieldFill.INSERT)
+    @TableField(value = "create_user", fill = FieldFill.INSERT)
     private Long createUser;
 
     @ApiModelProperty("修改人")
-    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @TableField(value = "update_user", fill = FieldFill.INSERT_UPDATE)
     private Long updateUser;
 
     @ApiModelProperty("创建时间")
-    @TableField(fill = FieldFill.INSERT)
+    @TableField(value = "create_time", fill = FieldFill.INSERT)
     private LocalDateTime createTime;
 
     @ApiModelProperty("修改时间")
-    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updateTime;
+
+    @ApiModelProperty("删除标志，0正常 1删除")
+    @TableField("del_flag")
+    private Integer delFlag;
 
 }
