536c9f0082775b2020d1b4415bb882d0b0e82132
cxs <437993390@qq.com>
2023-05-17 00:23:09 +0800

    fix: fastjson2 dependency version and code optimize

--- barren-core/barren-starter-auth/src/main/java/org/barren/core/auth/utils/AuthUtil.java
+++ barren-core/barren-starter-auth/src/main/java/org/barren/core/auth/utils/AuthUtil.java
@@ -1,7 +1,7 @@
 package org.barren.core.auth.utils;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.jwt.Jwt;
--- barren-core/barren-starter-auth/src/main/java/org/barren/core/auth/utils/SecurityUtil.java
+++ barren-core/barren-starter-auth/src/main/java/org/barren/core/auth/utils/SecurityUtil.java
@@ -1,6 +1,6 @@
 package org.barren.core.auth.utils;
 
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 
 /**
