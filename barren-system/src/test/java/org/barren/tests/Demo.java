fd81ebe6b303820e67e966289a887f10abfb0f2c
cxs <437993390@qq.com>
2023-11-24 22:20:07 +0800

    test: 测试 CompletableFuture

--- barren-system/src/test/java/org/barren/tests/Demo.java
+++ barren-system/src/test/java/org/barren/tests/Demo.java
@@ -1,14 +1,15 @@
 package org.barren.tests;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import lombok.extern.slf4j.Slf4j;
-import org.barren.modules.system.entity.SysMenu;
 import org.barren.modules.system.entity.SysUser;
-import org.barren.modules.system.service.ISysMenuService;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.Stream;
 
 /**
  * 单纯测试
@@ -59,75 +60,41 @@ public class Demo {
     }
 
 
-    public static void main(String[] args) {
-        List<SysMenu> menuList = new ArrayList<>();
-        SysMenu menu1 = new SysMenu();
-        menu1.setId(1L);
-        menu1.setPid(null);
-        menu1.setName("1111");
-        menu1.setSort(1);
+    @Test
+    public void testStream(){
+        // Streams 静态方法：of()、iterate()、generate()
+        Stream<Integer> stream1 = Stream.of(1, 3, 4, 6, 7, 8);
+        stream1.forEach(System.out::println);
 
-        SysMenu menu2 = new SysMenu();
-        menu2.setId(2L);
-        menu2.setPid(null);
-        menu2.setName("2222");
-        menu2.setSort(2);
+        Stream<Integer> stream2 = Stream.iterate(1, x -> x + 2).limit(4);
+        stream2.forEach(System.out::println);
 
-        SysMenu menu3 = new SysMenu();
-        menu3.setId(3L);
-        menu3.setPid(null);
-        menu3.setName("2222");
-        menu3.setSort(3);
+        Stream<Double> stream3 = Stream.generate(Math::random).limit(5);
+        stream3.forEach(System.out::println);
 
-        SysMenu menu4 = new SysMenu();
-        menu4.setId(4L);
-        menu4.setPid(2L);
-        menu4.setName("4444");
-        menu4.setSort(1);
+        LinkedBlockingQueue<Object> linkedBlockingQueue = new LinkedBlockingQueue<>(1024);
+        ArrayBlockingQueue arrayBlockingQueue = new ArrayBlockingQueue<>(1024);
 
-        SysMenu menu5 = new SysMenu();
-        menu5.setId(5L);
-        menu5.setPid(3L);
-        menu5.setName("5555");
-        menu5.setSort(1);
 
-        SysMenu menu6 = new SysMenu();
-        menu6.setId(6L);
-        menu6.setPid(3L);
-        menu6.setName("6666");
-        menu6.setSort(2);
 
-        SysMenu menu7 = new SysMenu();
-        menu7.setId(7L);
-        menu7.setPid(3L);
-        menu7.setName("7777");
-        menu7.setSort(3);
 
-        menuList.add(menu1);
-        menuList.add(menu2);
-        menuList.add(menu3);
-        menuList.add(menu4);
-        menuList.add(menu5);
-        menuList.add(menu6);
-        menuList.add(menu7);
-
-        // TreeNodeConfig treeNodeConfig = new TreeNodeConfig();
-        // // 自定义属性名 否则都是默认值
-        // treeNodeConfig.setIdKey("id");
-        // treeNodeConfig.setParentIdKey("pid");
-        // treeNodeConfig.setWeightKey("sort");
-        //
-        // List<Tree<Long>> list = TreeUtil.build(menuList, null, treeNodeConfig, (treeNode, tree) -> {
-        //     tree.setId(treeNode.getId());
-        //     tree.setParentId(treeNode.getPid());
-        //     tree.setWeight(treeNode.getSort());
-        //     tree.setName(treeNode.getName());
-        // });
-        // System.out.println(JSON.toJSONString(list, true));
-        // String jsonString = JSON.toJSONString(list, true);
-        // List<SysMenu> sysMenus = JSON.parseArray(jsonString, SysMenu.class);
-        // System.out.println("----------------");
-        // System.out.println(JSON.toJSONString(list, true));
-        System.out.println(JSON.toJSONString(ISysMenuService.buildTree(menuList), true));
     }
+
+    public static void main(String[] args) throws UnknownHostException {
+        // HttpStatus.
+        // CompletableFuture<Void> voidCompletableFuture = CompletableFuture.runAsync(() -> {
+        //
+        //
+        //     CompletableFuture.allOf(null).thenRun(null);
+        // }, Executors.newFixedThreadPool(5));
+        // FutureTask<?> task = new FutureTask<>(()->{
+        //     TimeUnit.SECONDS.sleep(1);
+        //     return "123";
+        // });
+        // CompletableFuture.
+        System.out.println(InetAddress.getLocalHost().getHostName());
+
+    }
+
+
 }
