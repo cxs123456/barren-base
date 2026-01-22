585eede2c91d5228885c67391cf4c3bff8736d02
cxs <437993390@qq.com>
2026-01-22 14:04:23 +0800

    fix: code optimize

--- barren-tools/barren-starter-sms/src/main/java/org/barren/tools/sms/AliSmsTemplate.java
+++ barren-tools/barren-starter-sms/src/main/java/org/barren/tools/sms/AliSmsTemplate.java
@@ -1,7 +1,7 @@
 package org.barren.tools.sms;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.aliyuncs.CommonRequest;
 import com.aliyuncs.CommonResponse;
 import com.aliyuncs.IAcsClient;
